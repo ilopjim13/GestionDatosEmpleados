ed2d4672344eed69bf3a51bf2e7b596fb912c80e
ivanjim13 <ivangorrion57@gmail.com>
2024-10-04 01:13:08 +0200

    terminado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,22 @@
 package org.example
 
 import org.example.repository.EmpleadoRepository
+import org.example.repository.XmlRepository
 import java.nio.file.Path
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
     val fileCsv = Path.of("src/main/resources/empleados.csv")
+    val fileXml = Path.of("src/main/resources/empleados.xml")
 
     val repository = EmpleadoRepository(fileCsv)
-
     val empleados = repository.generateEmpleados()
+
+    val xmlRepository = XmlRepository(fileXml)
+    xmlRepository.empleadosXml(empleados)
+
+    xmlRepository.empleadosXml(empleados, 1, 1333.3)
+
+    xmlRepository.readXml()
 
 }
--- src/main/kotlin/repository/XmlRepository.kt
+++ src/main/kotlin/repository/XmlRepository.kt
@@ -1,23 +1,26 @@
 package org.example.repository
 
 import org.example.model.Empleado
+import org.w3c.dom.Element
+import org.w3c.dom.Node
 import java.nio.file.Path
 import javax.xml.parsers.DocumentBuilderFactory
-import javax.xml.transform.Source
+import javax.xml.transform.OutputKeys
+import javax.xml.transform.TransformerFactory
 import javax.xml.transform.dom.DOMSource
 import javax.xml.transform.stream.StreamResult
 
-class XmlRepository {
+class XmlRepository(private val fileXml:Path) {
 
-    fun createXml(listaEmpleados:MutableList<Empleado>) {
-
+    fun empleadosXml(listaEmpleados: List<Empleado>, id:Int = -1, sal:Double = -1.0) {
         val dbf = DocumentBuilderFactory.newInstance()
         val db = dbf.newDocumentBuilder()
         val dip = db.domImplementation
         val document = dip.createDocument(null, "empleados", null)
 
         listaEmpleados.forEach {
-            val empleado = document.createElement("empleado id=´${it.id}´")
+            val empleado = document.createElement("empleado")
+            empleado.setAttribute("id", it.id.toString())
             document.documentElement.appendChild(empleado)
 
             val apellido = document.createElement("apellido")
@@ -29,7 +32,7 @@ class XmlRepository {
 
             val textApellido = document.createTextNode(it.apellido)
             val textDepartamento = document.createTextNode(it.departamento)
-            val textSalario = document.createTextNode(it.salario.toString())
+            val textSalario = if(it.id == id && sal >= 0) document.createTextNode(sal.toString()) else document.createTextNode(it.salario.toString())
 
             apellido.appendChild(textApellido)
             departamento.appendChild(textDepartamento)
@@ -37,8 +40,35 @@ class XmlRepository {
         }
 
         val source = DOMSource(document)
-        //val result = StreamResult(Path.of(""))
+        val result = StreamResult(fileXml.toFile())
 
+        val transformer = TransformerFactory.newInstance().newTransformer()
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes")
+
+        transformer.transform(source, result)
+    }
+
+    fun readXml() {
+        val dbf = DocumentBuilderFactory.newInstance()
+        val db = dbf.newDocumentBuilder()
+        val document = db.parse(fileXml.toFile())
+        val root = document.documentElement
+        root.normalize()
+
+        val listaNodos = root.getElementsByTagName("empleado")
+        for (i in 0..<listaNodos.length ) {
+            val nodo = listaNodos.item(i)
+
+            if (nodo.nodeType == Node.ELEMENT_NODE) {
+                val nodoElement = nodo as Element
+                val id = nodoElement.getAttribute("id")
+                val apellido = nodoElement.getElementsByTagName("apellido").item(0).textContent
+                val departamento = nodoElement.getElementsByTagName("departamento").item(0).textContent
+                val salario = nodoElement.getElementsByTagName("salario").item(0).textContent
+
+                println("ID: $id, Apellido: $apellido, Departamento: $departamento, Salario: $salario")
+            }
+        }
     }
 
 }
